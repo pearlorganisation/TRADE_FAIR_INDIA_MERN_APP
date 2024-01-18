271ae90ba955d3d23d035e6c45c7172018a0e058
Abhishek Bahuguna <abhishek@pearlorganisation.com>
2024-01-18 16:59:26 +0530

    added loader verify email button

--- frontend/src/pages/EmailVerification/EmailVerification.js
+++ frontend/src/pages/EmailVerification/EmailVerification.js
@@ -6,7 +6,9 @@ import { emailVerification } from "../../features/actions/authAction";
 
 const EmailVerification = () => {
   const { token, id } = useParams();
-  const { authData, isEmailVerified } = useSelector((state) => state.auth);
+  const { authData, isEmailVerified, isLoading } = useSelector(
+    (state) => state.auth
+  );
   const dispatch = useDispatch();
   const navigate = useNavigate();
   console.log("token::", token);
@@ -25,15 +27,25 @@ const EmailVerification = () => {
           Thanks for Signing up.
           <br /> Please Click Verify button to verify your email.
         </p>
-        <button
-          onClick={() => {
-            dispatch(emailVerification({ token, id }));
-          }}
-          className="px-5 py-2 bg-[#00373E] text-white rounded hover:bg-[#00373E]/90 active:bg-[#00373E]/80 active:scale-95 transition-all"
-          type="button"
-        >
-          Verify
-        </button>
+        {isLoading ? (
+          <button
+            type="button"
+            className="px-5 py-2 bg-[#00373E] text-white rounded hover:bg-[#00373E]/90 active:bg-[#00373E]/80 active:scale-95 transition-all flex justify-center items-center gap-2"
+          >
+            Loading...
+            <div class="w-5 h-5 border-4 rounded-full border-t-white/30 animate-spin"></div>
+          </button>
+        ) : (
+          <button
+            onClick={() => {
+              dispatch(emailVerification({ token, id }));
+            }}
+            className="px-5 py-2 bg-[#00373E] text-white rounded hover:bg-[#00373E]/90 active:bg-[#00373E]/80 active:scale-95 transition-all"
+            type="button"
+          >
+            Verify
+          </button>
+        )}
       </div>
     </div>
   );
